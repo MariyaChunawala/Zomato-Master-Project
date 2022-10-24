f1a3c55bc06d0e895811ba1cc02ac4dd97a36ce3
unknown <mariyachunawala7@gmail.com>
2022-10-25 00:59:40 +0530

    Add Menu APIs

--- server/API/Menu/index.js
+++ server/API/Menu/index.js
@@ -1,15 +1,40 @@
 import express from 'express';
 
-import { MenuModel } from '../../Database/allModels';
+import { ImageModel, MenuModel } from '../../Database/allModels';
 
 const Router = express.Router();
 /* 
-    Route: /r
-    Description: Get all food based on particular restaurant
-    Parameters: id
+    Route: /list
+    Description: Get all list Menu based on restaurant id
+    Parameters: _id
     Access: Public
     Method: GET
 */
+Router.get("/list/:_id", async (request, response) => {
+    try {
+        const { _id } = request.params;
+        const menus = await MenuModel.findOne({ _id });
+        return response.status(200).json({ menus });
+    } catch (error) {
+        return response.status(500).json({ error: error.message });
+    }
+})
 
+/* 
+    Route: /image
+    Description: Get all image Menu based on restaurant id
+    Parameters: _id
+    Access: Public
+    Method: GET
+*/
+Router.get("/image/:_id", async (request, response) => {
+    try {
+        const { _id } = request.params;
+        const menus = await ImageModel.findOne({ _id });
+        return response.status(200).json({ menus });
+    } catch (error) {
+        return response.status(500).json({ error: error.message });
+    }
+});
 
 export default Router;
